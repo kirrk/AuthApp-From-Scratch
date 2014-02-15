7b98de056ae6488c5098e3ec92575539e6e8e5f9
Karen Kirr <kkirr77@yahoo.com>
2014-02-14 19:02:42 -0800

    update with auth app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuthApp::Application.config.secret_key_base = '267e7bc8390355ca468f7c841ebf25cee870c3673b87db2b3cb02d3f320966ccd26de23746870edc1cbb006c7f0a04de312948a6fe5b3e751fe535a2b6ba8e71'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AuthApp::Application.config.session_store :cookie_store, key: '_AuthApp_session'
